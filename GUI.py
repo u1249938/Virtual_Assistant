25dc099b5f6361362851910f4358771cdaea0917
u1249938 <u1249938@utah.edu>
2022-06-11 11:16:07 -0600

    set up the window

--- GUI.py
+++ GUI.py
@@ -9,6 +9,7 @@ root = Tk()
 app = Window(root)
 
 root.geometry("400x400")
-root.attributes('-alpha', 0.5) # Not transparent enough
+root.wait_visibility(root)
+root.attributes('-alpha', 0.01) # Not transparent enough
 root.wm_title("Virtual Assistant")
 root.mainloop()
