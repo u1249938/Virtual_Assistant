0e72de9629524b06754a4f9c2102f09c726542a1
u1249938 <u1249938@utah.edu>
2022-06-21 22:49:56 -0600

    added a button and a icon to go along with it

--- GUI.py
+++ GUI.py
@@ -1,4 +1,7 @@
+from calendar import calendar
 from tkinter import *
+
+# Calender icon from Freepik
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -8,8 +11,23 @@ class Window(Frame):
 root = Tk()
 app = Window(root)
 
-root.geometry("400x400")
+root.geometry("1200x800")
 root.wait_visibility(root)
-root.attributes('-alpha', 0.01) # Not transparent enough
 root.wm_title("Virtual Assistant")
+root.resizable(False, False)
+root['bg'] = '#0a0a0a'
+
+# Calender Button Frame
+
+calBtnFrame = Frame(root)
+calBtnFrame.pack()
+
+def setWindow():
+    print('clicked')
+
+calenderIcon = PhotoImage(file = 'Images\calendar.png')
+    
+calenderBtn = Button(calBtnFrame, image = calenderIcon, command = setWindow, bg = '#0a0a0a', highlightthickness = 0, bd = 0)
+calenderBtn.pack()
+
 root.mainloop()
